3153953d1c65c42ceac42928f7a489ee60d0bdf9
clbutl <cannonlbutler@gmail.com>
2022-12-07 09:22:00 -0500

    fixed some bugs, fixed logo not showing up

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -33,6 +30,7 @@ module.exports = () => {
         theme_color: '#ffffff',
         start_url: './',
         publicPath: './',
+        fingerprints: false,
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
